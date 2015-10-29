d9653098b29891e3cc952f136ec16d35f810d79e
Anton Kosinov <a.s.kosinov@gmail.com>
2015-10-29 16:27:37 +0200

    Init OOP model

---
+++ models.py
@@ -0,0 +1,14 @@
+"""
+All necessary data models are here
+"""
+
+class Product():
+    """
+    iHerb product
+    """
+    def __init__(self):
+        self.name = ''
+        self.t_img = ''
+        self.s_img = ''
+        self.l_img = ''
+
